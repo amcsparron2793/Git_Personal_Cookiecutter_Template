23db5adc7acd804fa032215cacaf1afce300985d
amcsparron2793 <amcsparron2793@users.noreply.github.com>
2021-01-24 10:30:29 -0500

    init commit

---
+++ {{cookiecutter.directory_name}}/{{cookiecutter.Modules}}/{{cookiecutter.file_name}}.py
@@ -0,0 +1,18 @@
+"""
+{{cookiecutter.directory_name}}
+"""
+
+
+# imports
+
+# globals
+
+# function definitions
+
+# main func
+def main_func():
+    pass
+
+
+if __name__ == '__main__':
+    main_func()
